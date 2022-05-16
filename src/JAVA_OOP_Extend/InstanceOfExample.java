47f6f4b8c9ea0698c5613477fa6dd3cffa5c0d34
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-05-16 09:40:45 +0900

    2022-05-13

--- src/JAVA_OOP_Extend/InstanceOfExample.java
+++ src/JAVA_OOP_Extend/InstanceOfExample.java
@@ -7,14 +7,14 @@ class Cat extends Animal{}
 public class InstanceOfExample {
     public static void main(String[] args) {
         Animal animal = new Animal();
-        System.out.println(animal instanceof Object); //상위 클래스
-        System.out.println(animal instanceof Animal); //해당 클래스
-        System.out.println(animal instanceof Bat); //하위 클래스
+        System.out.println(animal instanceof Object);
+        System.out.println(animal instanceof Animal);
+        System.out.println(animal instanceof Bat);
 
-        Animal cat = new Cat(); //업 캐스팅
-        System.out.println(cat instanceof Object); //상위 클래스
-        System.out.println(cat instanceof Animal); //상위 클래스
-        System.out.println(cat instanceof Cat); //해당 클래스
-        System.out.println(cat instanceof Bat); //공통된 상위클래스를 둔 또 다른 하위 클래스
+        Animal cat = new Cat();
+        System.out.println(cat instanceof Object);
+        System.out.println(cat instanceof Animal);
+        System.out.println(cat instanceof Cat);
+        System.out.println(cat instanceof Bat);
     }
 }
