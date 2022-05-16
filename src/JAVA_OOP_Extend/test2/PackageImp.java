cc7a547da533a6775891a6867db5729668d81ad4
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-05-16 09:36:35 +0900

    2022-05-12

--- src/JAVA_OOP_Extend/test2/PackageImp.java
+++ src/JAVA_OOP_Extend/test2/PackageImp.java
@@ -1,8 +1,7 @@
 package JAVA_OOP_Extend.test2;
 
-public class PackageImp { //패키지 Import X
+public class PackageImp {
     public static void main(String[] args) {
-        //패키지명.클래스로 패키지명을 모두 명시해야 합니다.
         JAVA_OOP_Extend.test1.ExampleImp example = new JAVA_OOP_Extend.test1.ExampleImp();
         example.print();
     }
