e6c2fe731d09dd8601702422ee36ece4a721a10b
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-05-16 09:44:56 +0900

    2022-05-12

--- src/JAVA_OOP_Extend/SuperTest.java
+++ src/JAVA_OOP_Extend/SuperTest.java
@@ -1,14 +1,14 @@
 package JAVA_OOP_Extend;
 
-class Human{ //상위 클래스
-    Human(){ //super()
+class Human{
+    Human(){
         System.out.println("휴먼 클래스 생성자");
     }
 }
 
-class Student extends Human{ //하위 클래스
-    Student(){ //this()
-        super(); //Human() 으로
+class Student extends Human{
+    Student(){
+        super();
         System.out.println("학생 클래스 생성자");
     }
 }
