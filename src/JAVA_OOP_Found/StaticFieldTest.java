058a7f0f2e162dae75c4940b5129bdd33dc990c6
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-05-16 09:35:24 +0900

    2022-05-10

--- src/JAVA_OOP_Found/StaticFieldTest.java
+++ src/JAVA_OOP_Found/StaticFieldTest.java
@@ -5,16 +5,16 @@ public class StaticFieldTest {
         StaticField staticField1 = new StaticField();
         StaticField staticField2 = new StaticField();
 
-        staticField1.num1 = 100; //인스턴스 변수 할당
+        staticField1.num1 = 100;
         staticField2.num1 = 1000;
 
-        System.out.println(staticField1.num1); //인스턴스 변수 출력
+        System.out.println(staticField1.num1);
         System.out.println(staticField2.num1);
 
         staticField1.num2 = 150;
         staticField2.num2 = 1500;
 
-        System.out.println(staticField1.num2); //클래스 변수 출력
+        System.out.println(staticField1.num2);
         System.out.println(staticField2.num2);
         System.out.println(StaticField.num2);
     }
