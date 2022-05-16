3c63680e700de90d28156ce6fed7613d23a651e6
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-05-16 09:33:10 +0900

    2022-05-11

--- src/JAVA_OOP_Found/ConstructorExample.java
+++ src/JAVA_OOP_Found/ConstructorExample.java
@@ -2,22 +2,22 @@ package JAVA_OOP_Found;
 
 public class ConstructorExample {
     public static void main(String[] args){
-        Constructor constructor1 = new Constructor(); //(1)
-        Constructor constructor2 = new Constructor("Hello World"); //(2)
-        Constructor constructor3 = new Constructor(5, 10); //(3)
+        Constructor constructor1 = new Constructor();
+        Constructor constructor2 = new Constructor("Hello World");
+        Constructor constructor3 = new Constructor(5, 10);
     }
 }
 
 class Constructor{
-    Constructor(){ //(1)
+    Constructor(){
         System.out.println("1번 생성자");
     }
 
-    Constructor(String str){ //(2)
+    Constructor(String str){
         System.out.println("2번 생성자");
     }
 
-    Constructor(int a, int b){ //(3)
+    Constructor(int a, int b){
         System.out.println("3번 생성자");
     }
 }
