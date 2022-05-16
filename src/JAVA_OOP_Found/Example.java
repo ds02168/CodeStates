219f35c7255bad9c281f6447f19c63c5cd1184bb
ds02168 <84536269+ds02168@users.noreply.github.com>
2022-05-16 09:34:12 +0900

    2022-05-11

--- src/JAVA_OOP_Found/Example.java
+++ src/JAVA_OOP_Found/Example.java
@@ -1,13 +1,12 @@
 package JAVA_OOP_Found;
 
 public class Example {
-    public Example(){ //기본 생성자
-        //this(10); 시 Example(int x)호출
+    public Example(){
         System.out.println("Example의 기본 생성자 호출!");
     };
 
-    public Example(int x){ //매개변수 1개 생성자
-        this(); //기본 생성자 호출(매개 변수로 구분)
+    public Example(int x){
+        this();
         System.out.println("Example의 두 번째 생성자 호출!");
     }
 }
